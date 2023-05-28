9030220431a1a5ebf34ad4ed21b7af95d71009c6
mariefernandaisla <mariefernanda.islaaguilera@gmail.com>
2023-05-28 19:49:28 -0400

    si confirmo

---
+++ JS/PizzeriaDojo/Script.js
@@ -0,0 +1,21 @@
+function pizzaOven(masaDelgada, tipoCorteza, quesos, salsas) {
+    var pizza = {};
+    pizza.masaDelgada = masaDelgada;
+    pizza.tipoCorteza = tipoCorteza;
+    pizza.quesos = quesos;
+    pizza.salsas = salsas;
+    return pizza;
+}
+    
+var pizza = pizzaOven("estilo chicago", "tradicional", ["mozzarella", "pepperoni", "salchicha"], "tomate");
+console.log(pizza);
+var pizza2 = pizzaOven("hawaianna", "piña", ["mozzarellasimple", "aceituna", "salchicha"], "tomate");
+console.log(pizza2);
+var pizza3 = pizzaOven("española", "carnes", ["mozzarella4quesos", "pepperoni", "pollo"], "tomate");
+console.log(pizza3);
+var pizza4 = pizzaOven("italiana", "pimenton", ["mozzarellatriple", "salame", "carne"], "tomate");
+console.log(pizza4);
+
+
+
+
