d99c318a8f19f0c8f38f6bfd2af8dd49ab977153
mariefernandaisla <mariefernanda.islaaguilera@gmail.com>
2023-06-07 21:12:02 -0400

    si confirmo

--- JS/Hacer conexiones/script.js
+++ JS/Hacer conexiones/script.js
@@ -1,12 +1,23 @@
 
 var a = document.querySelector("a");
 var editprofile = document.querySelector("#usuarioSelect");
+
+
 document.querySelector('#usuarioSelect').addEventListener('change', function(event) {
     const editprofile = event.target.value;
     console.log('Adrien D.' + usuarioSelect);
     // Aquí puedes hacer lo que necesites con el usuario seleccionado
 }
 );
+
+function closecircle() {
+    console.log("entra")
+    var btn = document.querySelector(".user-line")
+
+    btn.remove();
+}
+
+
 
 function login(element) {
     if (element.innerText == "Login") {
